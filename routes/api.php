fca6ddd95d5e2b58d2b630c1b0baacf70bac041b
서상희 <tbvjaos510@naver.com>
2019-07-11 12:49:48 +0900

    [TEST] test commit

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Http\Request;
 
 Route::post('deploy', 'UtilController@deploy');
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
