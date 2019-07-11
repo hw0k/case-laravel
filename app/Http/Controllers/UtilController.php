322aebd3de9a2591a9dc79e2c61c7fa93638a336
서상희 <tbvjaos510@naver.com>
2019-07-11 16:01:17 +0900

    [ADD] token 검사 삭제

--- app/Http/Controllers/UtilController.php
+++ app/Http/Controllers/UtilController.php
@@ -5,25 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 
-
 class UtilController extends Controller
 {
     public function deploy(Request $request)
     {
-        $githubPayload = $request->getContent();
-        $githubHash = $request->header('X-Hub-Signature');
-
-        $localToken = config('app.deploy_secret');
-        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-
-        if ($githubHash != null && hash_equals($githubHash, $localHash)) {
-            echo 'equal';
-            $root_path = base_path();
-            $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-            $process->run(function ($type, $buffer) {
-                echo $type;
-                echo $buffer;
-            });
-        }
+        $root_path = base_path();
+        $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+        $process->run(function ($type, $buffer) {
+            echo $buffer;
+        });
     }
 }
